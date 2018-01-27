870dd15a603a98a9c8f846d338f16a821524f84a
Janice Lee <janicelee1120@gmail.com>
2018-01-27 17:38:47 -0500

    Started models file

--- project/journee/models.py
+++ project/journee/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+import datetime
+from datetime import date
+
+class Traveler(models.Model):
+	first_name = models.CharField
+	last_name = models.CharField
+	facebook_email = models.CharField
+
+
+
+class travelGroup(models.Model):
+	trip_name = models.Charfield
