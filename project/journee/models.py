b71cd11041e5eda4ac06d3509e145b5744a213bc
Janice Lee <janicelee1120@gmail.com>
2018-01-27 18:38:13 -0500

    Fixed typos cuz i sleepy

--- project/journee/models.py
+++ project/journee/models.py
@@ -6,11 +6,21 @@ import datetime
 from datetime import date
 
 class Traveler(models.Model):
-	first_name = models.CharField
-	last_name = models.CharField
-	facebook_email = models.CharField
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	facebook_username = models.CharField(max_length=50)
+
+class TravelGroup(models.Model):
+	members = models.ManyToMAnyField(Traveler)
+	trip_name = models.Charfield(max_length-50)
+	interested_points = models.ManyToManyField(PointOfInterest)
+	iternary = models.ManyToManyField(Event)
+
+class Event(models.Model):
+	date_event = models.DateField
+	hour = nodels.charfield(max_length=4)
+	place_id = models.CharField(max_length=50)
 
 
-
-class travelGroup(models.Model):
-	trip_name = models.Charfield
+class PointOfInterest(models.Model):
+	place_id = models.CharField(max_length=50)
