9b54eca755eb6214e677be116253d13521391c75
Cynthia Zhou <cyntzhou@gmail.com>
2018-01-27 20:28:22 -0500

    added to models

--- project/journee/views.py
+++ project/journee/views.py
@@ -4,16 +4,6 @@ from django.http import HttpResponse
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import os
-
-# flow = google_auth_oauthlib.flow.Flow.from_client_config(
-#     client_config = {"web":{"client_id":"680374767023-7v1uabqifrqh3ok9p8vktq4ef62k3js2.apps.googleusercontent.com","project_id":"brownbears18-193419","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"myZAS5-6DRxSF0vbKnAHyRG8","redirect_uris":["http://127.0.0.1:8000/oauth2callback"],"javascript_origins":["http://127.0.0.1:8000"]}},
-#     scopes=[])
-# 
-# flow.redirect_uri = 'http://127.0.0.1:8000/oauth2callback'
-# 
-# authorization_url, state = flow.authorization_url(
-#     access_type='offline',
-#     include_granted_scopes='true')
     
 SCOPES = []
 
