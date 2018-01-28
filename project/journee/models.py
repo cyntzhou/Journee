7ea386d4f7ac25c415a48a792fc76bec91b5949e
Janice Lee <janicelee1120@gmail.com>
2018-01-27 19:28:12 -0500

    Fixed bitty of django migration problem

--- project/journee/models.py
+++ project/journee/models.py
@@ -6,21 +6,23 @@ import datetime
 from datetime import date
 
 class Traveler(models.Model):
-	first_name = models.CharField(max_length=50)
-	last_name = models.CharField(max_length=50)
-	facebook_username = models.CharField(max_length=50)
-
-class TravelGroup(models.Model):
-	members = models.ManyToMAnyField(Traveler)
-	trip_name = models.Charfield(max_length-50)
-	interested_points = models.ManyToManyField(PointOfInterest)
-	iternary = models.ManyToManyField(Event)
-
-class Event(models.Model):
-	date_event = models.DateField
-	hour = nodels.charfield(max_length=4)
-	place_id = models.CharField(max_length=50)
-
+	first_name = models.CharField(max_length=50, null=True)
+	last_name = models.CharField(max_length=50, null=True)
+	facebook_username = models.CharField(max_length=50, null=True)
 
 class PointOfInterest(models.Model):
 	place_id = models.CharField(max_length=50)
+
+class Event(models.Model):
+	date_event = models.DateField
+	hour = models.CharField(max_length=4)
+	place_id = models.CharField(max_length=50)
+
+class TravelGroup(models.Model):
+	members = models.ManyToManyField(Traveler)
+	trip_name = models.CharField(max_length=50)
+	interested_points = models.ManyToManyField(PointOfInterest)
+	iternary = models.ManyToManyField(Event)
+
+
+
