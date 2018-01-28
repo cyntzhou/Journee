1eba26e6d109ca517e12ba9df31e25d1982895b9
Janice Lee <janicelee1120@gmail.com>
2018-01-27 22:44:51 -0500

    Models show up in admin

--- project/journee/admin.py
+++ project/journee/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Traveler, PointOfInterest, Event, Trip
 
 # Register your models here.
+
+admin.site.register(Traveler)
+admin.site.register(PointOfInterest)
+admin.site.register(Event)
+admin.site.register(Trip)
